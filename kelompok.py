812594f59e43089f0a8f3de3cbe65e9de6081088
kangzid <zidanalmubarok652@gmail.com>
2023-11-04 18:07:41 +0700

    fixlooping

--- kelompok.py
+++ kelompok.py
@@ -1,68 +1,87 @@
-rak = []
 bukuSudahDibaca = []
 bukuBelumDibaca = []
-
 
 def sudahDibaca(b):
     bukuSudahDibaca.append(b)
 
-
 def belumDibaca(b):
     bukuBelumDibaca.append(b)
 
-
 def rakBuku():
-    return {"bukuSudahDibaca": bukuSudahDibaca, "bukuBelumDibaca": bukuBelumDibaca}
+    return {
+        "bukuSudahDibaca": bukuSudahDibaca,
+        "bukuBelumDibaca": bukuBelumDibaca
+    }
 
+def hitungTotalBuku():
+    return len(bukuSudahDibaca) + len(bukuBelumDibaca)
 
-def hitungTotalBuku(bukuList):
-    if not bukuList:
-        return 0
-    else:
-        return 1 + hitungTotalBuku(bukuList[1:])
-
+def tampilkanSemuaBuku():
+    rak = rakBuku()
+    semuaBuku = rak["bukuSudahDibaca"] + rak["bukuBelumDibaca"]
+    print("|=======================================|")
+    print("Semua buku di rak: ")
+    for buku in semuaBuku:
+        print("- ", buku)
+    print("|=======================================|")
 
 while True:
-    print("RAK BUKU")
-    print("MENU")
-    print("1. Memasukkan buku ke rak")
-    print("2. Lihat semua buku")
-    print("3. Lihat buku yang sudah dibaca")
-    print("4. Lihat buku yang belum dibaca")
-    print("5. Hitung total buku di rak")
-    print("6. Keluar")
 
-    rak = rakBuku()
-    pilihMenu = int(input("Pilih menu (1-6): "))
+    print("  |}=====  MENU SYSTEM RAK BUKU ====={|")
+    print("=======================================")
+    print("| 1.|]  Memasukkan buku ke rak         [|")
+    print("| 2.|]  Lihat semua buku               [|")
+    print("| 3.|]  Lihat buku yang sudah dibaca   [|")
+    print("| 4.|]  Lihat buku yang belum dibaca   [|")
+    print("| 5.|]  Hitung total buku di rak       [|")
+    print("| 6.|]  Tampilkan semua buku           [|")
+    print("| 7.|]  Keluar                         [|")
+    print("=======================================")
+
+    pilihMenu = int(input("         Pilih menu (1-7): "))
+    print(".")
     if pilihMenu == 1:
+        print("|==================================|")
         buku = input("Masukkan judul buku: ")
         cek = input("Buku sudah dibaca/belum? (sudah/belum): ").lower()
         if cek == "sudah":
             sudahDibaca(buku)
-            print("Buku sudah dibaca: ", bukuSudahDibaca)
+            print("|==================================|")
+            print("  Buku sudah dibaca: ", bukuSudahDibaca)
+            print("|==================================|")
         elif cek == "belum":
             belumDibaca(buku)
-            print("Buku belum dibaca: ", bukuBelumDibaca)
+            print("|=======================================|")
+            print("  Buku belum dibaca: ", bukuBelumDibaca)
+            print("|=======================================|")
         else:
             print("Pilihan tidak valid.")
     elif pilihMenu == 2:
+        rak = rakBuku()
+        print("|=========================|")
         print("Semua buku: ", rak["bukuSudahDibaca"] + rak["bukuBelumDibaca"])
+        print("|=========================|")
     elif pilihMenu == 3:
-        print("buku sudah dibaca")
-        print("====================")
-        for bsd in bukuSudahDibaca:
-            print(bsd)
-        print("====================")
+        rak = rakBuku()
+        print("|=======================================|")
+        print("Buku yang sudah dibaca: ", rak["bukuSudahDibaca"])
+        print("|=======================================|")
     elif pilihMenu == 4:
-        print("buku belum dibaca")
-        print("====================")
-        for bbd in bukuBelumDibaca:
-            print(bbd)
-        print("====================")
+        rak = rakBuku()
+        print("|=======================================|")
+        print("Buku yang belum dibaca: ", rak["bukuBelumDibaca"])
+        print("|=======================================|")
     elif pilihMenu == 5:
-        totalBuku = hitungTotalBuku(rak["bukuSudahDibaca"] + rak["bukuBelumDibaca"])
-        print("Total buku di rak: ", totalBuku)
+        totalBuku = hitungTotalBuku()
+        print("|=========================|")
+        print("   Total buku di rak: ", totalBuku )
+        print("|=========================|")
     elif pilihMenu == 6:
+        tampilkanSemuaBuku()
+    elif pilihMenu == 7:
+        print("|=========================|")
+        print("|}   - Sampai jumpa -    {|")
+        print("|=========================|")
         break
     else:
         print("Pilihan tidak valid.")
